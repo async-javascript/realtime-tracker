7703f3c2cb4120d9989b80282759e4beddd8e469
Shivam Tiwari <shivam.tiwari20025@gmail.com>
2024-10-09 21:58:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+// Set the static files directory
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket) {
+    socket.on("send-location",function(data){
+        io.emit("receive-location",{id:socket.id,...data});
+    })
+    socket.on("disconnect",function(){
+        io.emit("user-disconnected",socket.id)
+    })
+    console.log("A user connected");
+
+    // You can add more socket event listeners here
+});
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+// Start the server
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
