59989f91649ebfdbe00ac022a5131da02dfab837
shivam.tiwari20025@gmail.com <shivam.tiwari20025@gmail.com>
2025-06-03 21:42:13 +0530

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,67 @@
-const socket = io()
+const socket = io();
+const map = L.map("map").setView([0, 0], 16);
 
+L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "OpenStreetMap"
+}).addTo(map);
+
+const markers = {};
+let myLocation = null;
+let routingControl = null;
+
+// Send your location
 if (navigator.geolocation) {
-    navigator.geolocation.watchPosition((position)=>{
-       const{latitude,longitude}= position.coords
-       socket.emit('send-location', {latitude,longitude})
-    }, (error)=>{
-        console.error(error)
-    },{
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        myLocation = [latitude, longitude];
+        socket.emit('send-location', { latitude, longitude });
+    }, (error) => {
+        console.error(error);
+    }, {
         enableHighAccuracy: true,
-        timeout:5000,
-        maximumAge:0
-    })
+        timeout: 5000,
+        maximumAge: 0
+    });
 }
 
-const map =L.map("map").setView([0,0],16)
+// Handle location updates
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    const userLocation = [latitude, longitude];
 
-L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution: "OpenStreetMap"
-}).addTo(map)
-
-const markers = {}
-
-socket.on("receive-location",(data)=>{
-    const {id , latitude,longitude}=data
-    map.setView([latitude,longitude])
-    if(markers[id]){
-        markers[id].setLatLng([latitude,longitude])
-    }else{
-        markers[id] = L.marker([latitude,longitude]).addTo(map)
+    if (!markers[id]) {
+        markers[id] = L.marker(userLocation).addTo(map);
+    } else {
+        markers[id].setLatLng(userLocation);
     }
-})
 
-socket.on("user-disconnected",(id)=>{
+    // Draw routing only to the first "other" user
+    if (myLocation && id !== socket.id) {
+        if (routingControl) {
+            routingControl.setWaypoints([myLocation, userLocation]);
+        } else {
+            routingControl = L.Routing.control({
+                waypoints: [
+                    L.latLng(myLocation),
+                    L.latLng(userLocation)
+                ],
+                routeWhileDragging: false
+            }).addTo(map);
+        }
+    }
+
+    map.setView(userLocation); // Optional: center map
+});
+
+// Remove user marker and route when they disconnect
+socket.on("user-disconnected", (id) => {
     if (markers[id]) {
-        map.removeLayer(markers[id])
-        delete markers[id]
+        map.removeLayer(markers[id]);
+        delete markers[id];
+
+        // If route was to this user, remove it
+        if (routingControl) {
+            routingControl.setWaypoints([]);
+        }
     }
-})
+});
